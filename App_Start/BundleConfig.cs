16d8db4e1b3be58f02b368cc443b3af611fd6f65
Matias Miraballes <matiasmiraballes@hotmail.com>
2019-07-25 20:49:45 -0300

    Cambio de theme bootstrap, Barra de navegación movida a Partial View

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace BlogDeInvestigacion
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lux.css",
                       "~/Content/site.css"));
         }
     }
